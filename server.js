83d3e79307982af0ff14eb4cafa845f47dc5045e
BlaiseAK <blaise.a.kress@gmail.com>
2018-07-12 20:45:10 -0400

    finally got localhost working and have the most of the routes completed

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-var express = ("express");
-var bodyParser = ("body-parser");
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.jsoon());
+app.use(bodyParser.json());
 
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
