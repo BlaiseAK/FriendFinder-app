c70b50992075be362453ea2b5d0e3981a6fe28a1
BlaiseAK <blaise.a.kress@gmail.com>
2018-07-12 23:52:53 -0400

    added all items into the app save for the post feature that provides the match

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,37 @@
 var funBuddyArry = [
     {
-        name: 'Blaise',
+        name: 'Greg',
         scores: [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+    },{
+        name: 'Mary',
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },{
+        name: 'Charlie',
+        scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+    },{
+        name: 'Jane',
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },{
+        name: 'Joe',
+        scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },{
+        name: 'Lindsey',
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },{
+        name: 'Jordan',
+        scores: [1, 2, 3, 4, 5, 5, 5, 4, 3, 2]
+    },{
+        name: 'George',
+        scores: [1, 1, 1, 1, 2, 2, 2, 3, 3, 4]
+    },{
+        name: 'Lauren',
+        scores: [2, 2, 2, 2, 4, 4, 4, 1, 1, 5]
+    },{
+        name: 'Tyler',
+        scores: [5, 1, 4, 2, 3, 3, 4, 5, 1, 1]
+    },{
+        name: 'Taylor',
+        scores: [5, 5, 5, 4, 5, 5, 5, 5, 1, 5]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,6 +12,19 @@ module.exports = function(app) {
     app.post("/api/funbuddies", function(req, res){
         res.push(req.body);
 
+        // The ending result of this was not completed to find the persons best match.
+
+        // capture the new fun buddy object to match against
+
+        // Now we need to make variables to match against like the matches name and difference (no image since I haven't set that up in the objects)
+
+        // create a loop that will help find which of the funbuddies is closest
+
+        // compute differences for each question
+
+        // if lowest difference then record it to the established variables
+
+        // done allow information to be called in the get to tell the user their fun buddies name
          
     })
 }
