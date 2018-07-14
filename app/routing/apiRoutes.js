c6f0d9172710ae33f9aeb78fed02572fee9570ed
BlaiseAK <blaise.a.kress@gmail.com>
2018-07-14 18:16:28 -0400

    got the post working however the logic is off all 1s should be leading to Lindsey but Tyler is the choice

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,23 +8,50 @@ module.exports = function(app) {
     app.get("/api/funbuddies", function(req, res) {
         res.json(friendsdata)
     });
-
+    
     app.post("/api/funbuddies", function(req, res){
-        res.push(req.body);
-
-        // The ending result of this was not completed to find the persons best match.
-
-        // capture the new fun buddy object to match against
-
-        // Now we need to make variables to match against like the matches name and difference (no image since I haven't set that up in the objects)
-
-        // create a loop that will help find which of the funbuddies is closest
-
-        // compute differences for each question
-
-        // if lowest difference then record it to the established variables
 
         // done allow information to be called in the get to tell the user their fun buddies name
-         
-    })
+        friendsdata.push(req.body);         
+        
+        // capture the new fun buddy object to match against
+        var newBuddyScores = req.body.scores;
+        console.log("the newBuddyScores: "+ newBuddyScores);
+        // Create an array to store all the differences
+        var potentialsDiffArry = [];
+        // Used to indicate at what index is the best match from the friendsdata variable
+        var closestMatch = 0;
+
+
+        for (var i = 0; i <friendsdata.length ; i++) {
+            var scoresDiff = 0;
+            for (var j = 0; j <newBuddyScores.length; j++) {
+                // compute differences for each question
+                scoresDiff += Math.abs(parseInt(newBuddyScores[j]) - parseInt(friendsdata[i].scores[j]));
+                console.log("Adding the diff"+scoresDiff)
+            }
+            // push the score difference to the array for the match index
+            potentialsDiffArry.push(scoresDiff);
+            console.log("scoresDiff"+scoresDiff+"for indexed: "+i);
+            
+        }
+            console.log("list of potentials: "+potentialsDiffArry)
+        // Now we need to make variables to match against like the matches name and difference (no image since I haven't set that up in the objects)
+        for (var i = 0; i<potentialsDiffArry.length; i++) {
+            // create a loop that will help find which of the funbuddies is closest
+            if (potentialsDiffArry[i]<potentialsDiffArry[i+1]){
+                // if lowest difference then record it to the established variables
+                closestMatch = i
+                console.log(closestMatch)
+            }
+            
+        }
+
+        // designate the the best match by giving the indexed object of friendsdata array
+        bestFunBuddy = friendsdata[closestMatch];
+
+        // send stuff back
+        res.json(bestFunBuddy);
+
+    });
 }
