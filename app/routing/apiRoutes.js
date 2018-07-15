eb936a9369b9c5f9d829b08b81ef0e5476459dfe
BlaiseAK <blaise.a.kress@gmail.com>
2018-07-15 16:17:30 -0400

    Well all items in the app now work! This is ready to be redeployed and regraded.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,53 +11,50 @@ module.exports = function(app) {
     
     app.post("/api/funbuddies", function(req, res){
 
-        // done allow information to be called in the get to tell the user their fun buddies name
-        friendsdata.push(req.body);         
-
+        
         // capture the new fun buddy object to match against
         var newBuddyScores = req.body.scores;
-
+        
         // Create an array to store all the differences
         var potentialsDiffArry = [];
         
         // Used to indicate at what index is the best match from the friendsdata variable
         var closestMatch = 0;
-
+        
 
         for (var i = 0; i < friendsdata.length ; i++) {
-        
+            
             var scoresDiff = 0;
-        
+            
             for (var j = 0; j <newBuddyScores.length; j++) {
-        
+                
                 // compute differences for each question
                 scoresDiff += Math.abs(parseInt(newBuddyScores[j]) - parseInt(friendsdata[i].scores[j]));
-                // console.log("Adding the diff"+scoresDiff)
-        
             }
-        
+            
             // push the score difference to the array for the match index
             potentialsDiffArry.push(scoresDiff);
-            console.log("scoresDiff"+scoresDiff+"for indexed: "+i);
             
         }
-            console.log("list of potentials: "+potentialsDiffArry)
+        console.log("list of potentials: "+potentialsDiffArry)
         // Now we need to make variables to match against like the matches name and difference (no image since I haven't set that up in the objects)
         for (var i = 0; i<potentialsDiffArry.length; i++) {
             // create a loop that will help find which of the funbuddies is closest
             if (potentialsDiffArry[i]<potentialsDiffArry[closestMatch]){
                 // if lowest difference then record it to the established variables
                 closestMatch = i
-                console.log(closestMatch)
             }
             
         }
-
+        
         // designate the the best match by giving the indexed object of friendsdata array
         bestFunBuddy = friendsdata[closestMatch];
-
+        
         // send stuff back
         res.json(bestFunBuddy);
-
+        
+        // done allow information to be called in the get to tell the user their fun buddies name
+        friendsdata.push(req.body);
+                 
     });
 }
